e5d43dbdffa32ddbb0f0d6947afb1e09834a32ae
Nick Battista <nickbattista@Nick.Battista>
2021-04-16 14:18:09 -0600

    added error handling.

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require("express")
+const express = require("express");
+const ExpressError = require("./expressError");
 const app = express();
 const listRoutes = require("./routes/list")
 
 app.use(express.json());
 app.use("/items", listRoutes);
+
+app.use((req, res, next) => {
+    return new ExpressError("Not found", 404);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+return res.json({
+    error: err.message
+});
+});
 
 module.exports = app;
--- routes/list.js
+++ routes/list.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = new express.Router()
 const items = require("../fakeDb")
+const ExpressError = require("../expressError")
+
 
 router.get('/', (req, res) => {
     res.json(items)
@@ -18,10 +20,29 @@ router.post('/', (req, res) => {
 router.get("/:item", (req,res) => {
     queryItem = items.find(item => item.item === req.params.item)
     if (queryItem === undefined) {
-        console.log("Item not found.")
+        throw new ExpressError("Item not found", 404)
     }
-    res.json(queryItem)
+
+    res.json(items)
 })
 
+router.patch("/:item", (req,res) => {
+    queryItem = items.find(item => item.item === req.params.item)
+    if (queryItem === undefined) {
+        throw new ExpressError("Cannot update. Item not found.", 404)
+    }
+    queryItem.item = req.body.item
+    queryItem.price = req.body.price
+    res.json(items)
+})
+
+router.delete("/:item", (req,res) => {
+    queryItem = items.findIndex(item => item.item === req.params.item)
+    if (queryItem === -1) {
+        throw new ExpressError("Cannot delete. Item not found.", 404)
+    }
+    items.splice(queryItem,1)
+    res.json(items)
+})
 
 module.exports = router;
